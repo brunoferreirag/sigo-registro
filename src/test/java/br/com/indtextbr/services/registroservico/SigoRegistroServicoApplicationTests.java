81f6baa01ef892ce73dd42c972c1cb302a220987
bags <bruno.goncalves@gft.com>
2020-10-22 15:55:31 -0300

    Versão inicial do registro de serviço do SIGO

---
+++ src/test/java/br/com/indtextbr/services/registroservico/SigoRegistroServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.indtextbr.services.registroservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigoRegistroServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
